f076b9475d040041372e19b02926f6befbd7c7b4
JaehyoJJAng <yshrim12@naver.com>
2023-02-21 15:25:10 +0900

    PORT -> PASSWORD 변경

--- 32_처음 배우는 쉘 스크립트 : 다수의 서버에 NTP 서버 설치하기/main.sh
+++ 32_처음 배우는 쉘 스크립트 : 다수의 서버에 NTP 서버 설치하기/main.sh
@@ -3,8 +3,8 @@
 # NTP 를 설치할 대상 서버정보 저장
 servers='db nfs'
 
-# Server Port
-PORT="${1}"
+# password 
+PASSWORD="${1}"
 
 # Command
 CMD1='cat /etc/*release | grep ID_LIKE | sed "s/ID_LIKE=//;s/\"//g"'
@@ -13,7 +13,7 @@ CMD2=''
 for server in ${servers}
 do
 	# 해당 서버의 운영체제 타입 확인
-	ostype=$(sshpass -p "${PORT}" ssh ${server} "${CMD1}"
+	ostype=$(sshpass -p "${PASSWORD}" ssh ${server} "${CMD1}"
 
 	# 운영체제가 Fedora 계열인지 Debian 계열인지 확인
 	if [[ ${ostype} == 'fedora' ]]
@@ -25,5 +25,5 @@ do
 	fi
 
 	# 해당 운영체제에 ntp 설치
-	sshpass -p ${PORT} ${server} "${CMD2}"
+	sshpass -p ${PASSWORD} ${server} "${CMD2}"
 done
